82964577ecc686bd3f08a1176f5dd9ff78ce3a95
HarisAli-git <harisa73@gmail.com>
2025-06-01 22:10:38 +0200

    Moved Navbar to Layout

--- fe/src/app/page.js
+++ fe/src/app/page.js
@@ -7,7 +7,6 @@ export default function Home() {
 
   return (
     <>
-      <Navbar />
       <div className="flex h-screen items-center justify-center bg-gray-100">
         <div className="p-8 bg-white shadow-lg rounded-xl text-center">
           <h1 className="text-2xl font-bold mb-6">

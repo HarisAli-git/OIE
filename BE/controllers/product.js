04822fc5f56965d9919c4bc796ebeb3c63c0a749
HarisAli-git <harisa73@gmail.com>
2025-06-02 14:08:31 +0200

    Added Search by pct_code or description

--- BE/controllers/product.js
+++ BE/controllers/product.js
@@ -1,12 +1,24 @@
 const { Product } = require("../models");
+const { Op } = require("sequelize");
 
 exports.getAllProducts = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1; // default: page 1
     const limit = parseInt(req.query.limit) || 10; // default: 10 items per page
+
+    const { pct_code, description } = req.query;
     const offset = (page - 1) * limit;
 
+    const whereClause = {};
+    if (pct_code) {
+      whereClause.pct_code = { [Op.like]: `%${pct_code}%` };
+    }
+    if (description) {
+      whereClause.description = { [Op.like]: `%${description}%` };
+    }
+
     const { count, rows: products } = await Product.findAndCountAll({
+      where: whereClause,
       offset,
       limit,
     });

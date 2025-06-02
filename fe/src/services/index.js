0d4be6e898566d3e9fbe0c42c8768a0d629f0b1b
HarisAli-git <harisa73@gmail.com>
2025-06-02 12:59:35 +0200

    Fixed env variable

--- fe/src/services/index.js
+++ fe/src/services/index.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const fetchProducts = async () => {
   try {
-    const response = await axios.get("/api/products");
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/product`
+    );
     return response.data;
   } catch (err) {
     return err;
